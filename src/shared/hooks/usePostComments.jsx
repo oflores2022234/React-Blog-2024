d4da9bea86ebff793547cc232aac87bbfa69e808
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-05-10 23:14:38 -0600

    start comments

--- src/shared/hooks/usePostComments.jsx
+++ src/shared/hooks/usePostComments.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postComentarios as postComentariosRequest } from "../../services/api";
@@ -5,11 +6,13 @@ import toast from "react-hot-toast";
 
 export const usePostComments = () => {
 
-    
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
 
   return (
     <div>Hola</div>
   )
 }
 
-
+*/
