e7425b96ddedb0d1f737017d307151b673be501a
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-05-10 23:50:24 -0600

    hook usePostComments

--- src/shared/hooks/usePostComments.jsx
+++ src/shared/hooks/usePostComments.jsx
@@ -1,18 +1,32 @@
-/*
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import { postComentarios as postComentariosRequest } from "../../services/api";
 import toast from "react-hot-toast";
 
 export const usePostComments = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  //const navigate = useNavigate();
 
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
+  const postComentarios = async (nombres, email, contenido, publicacionId) => {
+    setIsLoading(true);
 
+    const response = await postComentariosRequest({
+      nombres,
+      email,
+      contenido,
+      publicacionId,
+    });
 
-  return (
-    <div>Hola</div>
-  )
-}
+    setIsLoading(false);
+    if (response.error) {
+      return toast.error(
+        response.e?.response?.data || 'Ocurrió un error al enviar el comentario'
+      );
+    }
+  };
 
-*/
+  return {
+    postComentarios,
+    isLoading
+  };
+};
